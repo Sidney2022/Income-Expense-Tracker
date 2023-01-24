39931fd64035b00ecd195a6eecca4ba50733d0b0
Sidney2022 <uwayasidney@gmail.com>
2023-01-24 10:05:46 +0100

    static

--- income_expense_tracker/settings.py
+++ income_expense_tracker/settings.py
@@ -134,7 +134,8 @@ LOGIN_URL= '/auth/login'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [ os.path.join( BASE_DIR, 'static' ), ]
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
