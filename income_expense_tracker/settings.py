50d2478e4c7031cfa2497a3e3b5e5d881a4168d0
Sidney2022 <uwayasidney@gmail.com>
2023-01-24 09:47:18 +0100

    csrf-origin

--- income_expense_tracker/settings.py
+++ income_expense_tracker/settings.py
@@ -154,4 +154,4 @@ DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USR')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 FOREX_API_KEY = os.getenv('API_LAYER_KEY')
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS =["https://*.up.railway.app", "http://*.up.railway.app"]
