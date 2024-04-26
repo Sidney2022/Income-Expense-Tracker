1b05ade3df5ee98e97346ff34e6839fd72544875
Sidney2022 <uwayasidney@gmail.com>
2024-04-26 23:17:21 +0100

    statics error 500 reolved

--- staticfiles/js/stats.js
+++ staticfiles/js/stats.js
@@ -1,50 +1,154 @@
-const renderChart =(data, labels) => {
-    const ctx = document.getElementById('myChart');
-  
-    new Chart(ctx, {
-        type: 'doughnut',
+const selectIncomeRange = document.getElementById('income-form')
+const IncomeForm = document.getElementById('form-field')
+const expenseForm = document.getElementById('expense-form')
+const  selectExpenseRange = document.getElementById('expense-select-field')
+
+
+const renderChart1 = (data, labels) => {
+  const ctx = document.getElementById('myChart1');
+
+  new Chart(ctx, {
+    type: "pie",
     data: {
-        labels: labels,
-        datasets: [{
-            label: 'Amount spent',
-            data: data,
-            backgroundColor: //[]
-            [
-                'teal',
-                'green',
-                'yellow',
-                'blue',
-                'red',
-                'magenta',
-            ],
-           
-            borderWidth: 1
-          }]
+      labels: labels,
+      datasets: [
+        {
+          label: "Amount Earned",
+          data: data,
+          //[]
+          backgroundColor: [
+            "teal",
+            "green",
+            "yellow",
+            "blue",
+            "red",
+            "magenta",
+          ],
+
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          },
-          title:{
-            display:true,
-            text:'Expenses sorted by Category'
-          }
-      }
-    });
-}               
-const getChartData=() => {
-    fetch('/stats')
-    .then((res)=>res.json())
+      title: {
+        display: true,
+        text: "Income sorted by Sources",
+      },
+    },
+  });
+};
+
+const renderChart2 = (data, labels) => {
+  const ctx = document.getElementById('myChart2');
+
+  new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Amount spent",
+          data: data,
+          //[]
+          backgroundColor: [
+            "teal",
+            "green",
+            "yellow",
+            "blue",
+            "red",
+            "magenta",
+          ],
+
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      title: {
+        display: true,
+        text: "Expenses sorted by Category",
+      },
+    },
+  });
+};
+const getChartData = () => {
+  fetch("/income/stats")
+    .then((res) => res.json())
     .then((results) => {
+      const category_data = results.final_data;
+      const [labels, data] = [
+        Object.keys(category_data),
+        Object.values(category_data),
+      ];
+      renderChart1(data, labels);
+    });
+
+  fetch("/expense/stats")
+    .then((res) => res.json())
+    .then((results) => {
+      const category_data = results.final_data;
+      const [labels, data] = [
+        Object.keys(category_data),
+        Object.values(category_data),
+      ];
+      renderChart2(data, labels);
+    });
+};
+document.onload = getChartData();
+
+
+IncomeForm.addEventListener('submit',  (e) => {
+  e.preventDefault()
+  fetch('/income/stats', {
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     },
+    body : JSON.stringify({ time_range : selectIncomeRange.value }),
+    method : 'POST'
+  })
+  .then(res => res.json()) 
+  .then((results) => {
+    console.log(results)
         const category_data = results.final_data;
-        console.log(category_data)
         const [labels, data] = [
-            Object.keys(category_data),
-            Object.values(category_data)
-        ]
-        renderChart(data, labels)
-    })
-}
-document.onload=getChartData()
+          Object.keys(category_data),
+          Object.values(category_data),
+        ];
+        renderChart1(data, labels);
+      });      
+    
+})
+
+expenseForm.addEventListener('submit',  (e) => {
+  e.preventDefault()
+  fetch('/expense/stats', {
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     },
+    body : JSON.stringify({ time_range : selectExpenseRange.value }),
+    method : 'POST'
+  })
+  .then(res => res.json()) 
+  .then((results) => {
+    console.log(results)
+        const category_data = results.final_data;
+        const [labels, data] = [
+          Object.keys(category_data),
+          Object.values(category_data),
+        ];
+        renderChart2(data, labels);
+      });      
+    
+})
