d64f8b72b54fcefbec09402933a7879d00da19a8
Sidney2022 <uwayasidney@gmail.com>
2024-04-27 06:33:41 +0100

    changed reqs.txt

--- income_expense_tracker/urls.py
+++ income_expense_tracker/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-
 
 
 urlpatterns = [
@@ -17,7 +15,6 @@ urlpatterns = [
     path('api/', include('api_app.urls'))
 
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
